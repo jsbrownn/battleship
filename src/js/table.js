c1304d5afe15ecbb84306d053d71d0f23b43582b
Sergei Boreiko <hsvpass@gmail.com>
2022-01-07 16:46:47 +0300

    init: init project

---
+++ src/js/table.js
@@ -0,0 +1,4 @@
+const tableProp = {
+  cell : 10,
+  
+}
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+
+module.exports = {
+  mode: 'development',
+
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
+
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+  },
+
+  devtool: 'inline-source-map',
+  devServer: {
+    historyApiFallback: true,
+    open: true,
+    compress: true,
+    hot: true,
+    port: 3000,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+
+  plugins: [
+    // применять изменения только при горячей перезагрузке
+    new HtmlWebpackPlugin({
+      title: 'Battleship',
+      template: path.resolve(__dirname, './src/template.html'),
+      filename: 'index.html',
+    }),
+    new CleanWebpackPlugin(),
+]
+
+
+
+}
